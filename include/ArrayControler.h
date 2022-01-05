2b66aa5f434fb0a494281b006dabaffdd7fd196c
min4604 <mina4604sh@gmail.com>
2022-01-05 21:34:11 +0800

    sat1

---
+++ include/ArrayControler.h
@@ -0,0 +1,3 @@
+void randArray(int a[], int n);
+void rotateMatrix(int data[4][4], int count);
+void copyArray(int data[4][4], int src[4][4]);
---
+++ source/ArrayControler.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void randArray(int a[], int n)
+{
+
+}
+
+void rotateMatrix(int data[4][4], int count)
+{
+
+}
+void copyArray(int data[4][4], int src[4][4])
+{
+
+}
